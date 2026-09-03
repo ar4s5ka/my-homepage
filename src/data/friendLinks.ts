358e5338392a1dd9d07e7b50033c5892ecb8e078
arasuka <297030865+ar4s5ka@users.noreply.github.com>
2026-09-03 22:43:19 +0900

    リンク更新

--- src/data/friendLinks.ts
+++ src/data/friendLinks.ts
@@ -10,5 +10,17 @@ export const friendLinks: FriendLink[] = [
 		name: "トラマト",
 		url: "https://toramutton.me/",
 		description: "このサイト作るのにも使用した、サイト制作のブログを書いていらっしゃる方。ヒカm..",
-	},
+	},{
+		name: "lanips",
+		url: "https://lanips-homepage.vercel.app/",
+		description: "同クラなのに、リアルでは話したことない。",
+	},{
+		name: "しー",
+		url: "https://c-homepage.vercel.app/",
+		description: "ダーツすごすぎてこれ。",
+	},{
+		name: "かき",
+		url: "https://my-homepage-one-murex.vercel.app/",
+		description: "成績フェイカー",
+	}
 ];
